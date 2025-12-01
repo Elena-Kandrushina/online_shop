df94607fce683d36ce736a0db9052506b6af7571
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-12-01 19:43:01 +0300

    add all changes from catalog/

--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView, DetailView
 
 from catalog.models import Product
 
+class HomeTemplateView(TemplateView):
+    template_name = 'catalog/home.html'
 
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html')
 
-def contacts(request):
-    return render(request, 'contacts.html')
+class ContactsTemplateView(TemplateView):
+    template_name = 'catalog/contacts.html'
 
-def products_list(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'products_list.html', context)
+# def contacts(request):
+#     return render(request, 'contacts.html')
 
-def products_detail(request, pk):
-    product = get_object_or_404(Product, id=pk)
-    context = {'product': product}
-    return render(request, 'products_detail.html', context)
+class ProductListView(ListView):
+    model = Product
+
+# def products_list(request):
+#     products = Product.objects.all()
+#     context = {'products': products}
+#     return render(request, 'products_list.html', context)
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = 'product'
+
+# def products_detail(request, pk):
+#     product = get_object_or_404(Product, id=pk)
+#     context = {'product': product}
+#     return render(request, 'product_detail.html', context)
