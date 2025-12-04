04879ccc37966752dfec0ced4c8e82d44b1713c0
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-12-04 22:08:53 +0300

    add catalog

---
+++ catalog/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from .models import Product
+
+FORBIDDEN_WORDS = [
+    "казино",
+    "криптовалюта",
+    "крипта",
+    "биржа",
+    "дешево",
+    "бесплатно",
+    "обман",
+    "полиция",
+    "радар",
+]
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'image', 'category', 'price', 'is_available']
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Введите наименование'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Введите описание'})
+        self.fields['image'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Добавьте изображение'})
+        self.fields['price'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Введите цену'})
+        self.fields['is_available'].widget.attrs.update({'class': 'form-check-input'})
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name', '')
+        self._check_forbidden_words(name)
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description', '')
+        self._check_forbidden_words(description)
+        return description
+
+    def _check_forbidden_words(self, text):
+        if not text:
+            return
+        text_lower = text.lower()
+        for word in FORBIDDEN_WORDS:
+            if word in text_lower:
+                raise forms.ValidationError(
+                    f"Использование слова '{word}' запрещено."
+                )
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price is not None and price < 0:
+            raise forms.ValidationError("Цена не может быть отрицательной.")
+        return price
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView, DeleteView
 
+from catalog.forms import ProductForm
 from catalog.models import Product
 
 
@@ -21,3 +22,21 @@ class ProductDetailView(DetailView):
     model = Product
     context_object_name = 'product'
 
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_create.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_edit.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:products_list')
