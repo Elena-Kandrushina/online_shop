bb1de0bc3d6d6eee40fb84ce44799bc550725029
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-12-14 21:10:17 +0300

    add change app - catalog

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -54,11 +55,25 @@ class Product(models.Model):
         auto_now_add=True, verbose_name="Дата последнего изменения"
     )
     is_available = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=False)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='products',
+        verbose_name='Владелец',
+        null=True,
+        blank=True,
+)
 
     def __str__(self):
         return f"{self.name} {self.category}"
+
 
     class Meta:
         verbose_name = "продукт"
         verbose_name_plural = "продукты"
         ordering = ["category", "name"]
+        permissions = [
+            ("can_unpublish_product", "Can unpublish product"),
+            ("can_delete_product", "Can delete product"),
+        ]
